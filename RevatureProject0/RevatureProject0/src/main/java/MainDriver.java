02ca351f181b7f4950bd555563dab40ca1cb5dc5
Kevin Basta <kbasta1@toromail.csudh.edu>
2021-07-20 00:06:52 -0700

    Main Driver, Soon to be updated

--- RevatureProject0/RevatureProject0/src/main/java/MainDriver.java
+++ RevatureProject0/RevatureProject0/src/main/java/MainDriver.java
@@ -1,42 +1,63 @@
 import java.util.Scanner;
 
+import com.revature.dao.UserDao;
+import com.revature.dao.UserDaoDB;
 import com.revature.exceptions.InvalidCredentialsException;
 import com.revature.models.User;
 
+import com.revature.services.UserServices;
+
 public class MainDriver {
+
+	private static UserDao uDao = new UserDaoDB();
+
+	private static UserServices uServ = new UserServices(uDao);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		boolean finished = false; // controls loop shown in SocialHub Project
-		
+
 		User u1 = null;
-		while(!finished) {
-			if(u1 == null) {
-				System.out.println("Welcome to Bank17! Would you like to login or signup for a new account today?");
-				String choice = null;
-				if(choice.equals("login")) {
+
+		while (!finished) {
+			if (u1 == null) {
+				System.out.println("Welcome to Banking Services! For Login press 1, for Signup press 2.");
+				int choice = Integer.parseInt(input.nextLine());
+				if (choice == 1) {
 					System.out.println("Please enter your username: ");
 					String username = input.nextLine();
-					System.out.println("Please enter your password");
+					System.out.println("Please enter your password: ");
 					String password = input.nextLine();
 					try {
-						
-					} catch(InvalidCredentialsException e) {
-						System.out.println("Username or password is incorrect.");
+						u1 = uServ.signIn(username, password);
+						System.out.println("Welcome! " + u1.getFirstName());
+					} catch (Exception e) {
+						System.out.println("Incorrect username or password.");
 						finished = true;
 					}
 				} else {
-					System.out.println("In order to open a new account please enter the following.");
-					System.out.println("First Name: ");
-					String firstNam = input.nextLine();
-					System.out.println("Last Name: ");
-					String lastName = input.nextLine();
-					System.out.println("Username: ");
-					String userNam = input.nextLine();
-					System.out.println("Password: ");
-					String password2 = input.nextLine();
+					System.out.println("Please enter your first name: ");
+					String first = input.nextLine();
+					System.out.println("Please enter your last name: ");
+					String last = input.nextLine();
+					System.out.println("Please enter your username: ");
+					String uname = input.nextLine();
+					System.out.println("Please enter a password: ");
+					String password = input.nextLine();
+					try {
+						u1 = uServ.signUp(first, last, uname, password);
+						System.out.println("You may noew log in with the username: " + u1.getUserName());
+					} catch (Exception e) {
+						System.out.println("Sorry we could not process your request.");
+						System.out.println("Please try again later.");
+						finished = true;
+					}
 				}
+
+			} else {
+				System.out.println("To view list press 1, to create a transaction 2.");
+				int choice = Integer.parseInt(input.nextLine());
 			}
 		}
 	}
