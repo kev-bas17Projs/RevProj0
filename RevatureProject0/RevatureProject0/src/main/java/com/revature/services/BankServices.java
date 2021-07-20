060a2fc086d017184c662cda8de358a26e80a5bf
Kevin Basta <kbasta1@toromail.csudh.edu>
2021-07-20 00:05:08 -0700

    Bank, User, Account Services

--- RevatureProject0/RevatureProject0/src/main/java/com/revature/services/BankServices.java
+++ RevatureProject0/RevatureProject0/src/main/java/com/revature/services/BankServices.java
@@ -2,24 +2,47 @@ package com.revature.services;
 
 import com.revature.models.BankAccount;
 import com.revature.models.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.*;
+
+import com.revature.dao.BankAccountDao;
+import com.revature.exceptions.InvalidCredentialsException;
+import com.revature.exceptions.UserDoesNotExistException;
+import com.revature.exceptions.UserNameAlreadyExistsException;
+import com.revature.logging.*;
+
+
 
 public class BankServices {
 	
-	Transaction transaction;
-	BankAccount balance;
+	//Transaction transaction;
+	//BankAccount balance;
 	
-	public BankAccount withdraw(double balance) {
+	//public BankAccount withdraw(double balance) {
 		//transaction += balance;
-		return null;
-	}
+		//return null;
+	//}
 
-	public BankAccount deposit(double balance) {
+	//public BankAccount deposit(double balance) {
 		
-		return null;
+		//return null;
+	//}
+	
+	//public BankAccount transferFunds() {
+		//return null;//
+	
+	
+	private BankAccountDao bADao;
+	
+	public BankServices(BankAccountDao BA) {
+		bADao = BA;
+		
 	}
 	
-	public BankAccount transferFunds() {
-		return null;
-	}
-
+	
+	
+	
 }
+
